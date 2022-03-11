3fb2039df187dc5e0b2360e9bca0a7284bf384ea
Fernando Isaías García Aguirre <fernaisagaragu@gmail.com>
2022-03-11 15:24:54 -0600

    initial commit

---
+++ src/main/java/org/pechblenda/festusinvitationrest/FestusInvitationRestApplication.java
@@ -0,0 +1,13 @@
+package org.pechblenda.festusinvitationrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestusInvitationRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestusInvitationRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pechblenda/festusinvitationrest/FestusInvitationRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pechblenda.festusinvitationrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestusInvitationRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
